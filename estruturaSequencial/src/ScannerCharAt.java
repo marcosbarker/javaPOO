24b2cb858edf35d4f581f01d65c8529c4ad3feab
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-04-30 17:18:39 -0300

    add exercExemplosInit

---
+++ estruturaSequencial/src/ScannerCharAt.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ScannerCharAt {
+	
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+			
+	char x;
+	x = sc.next().charAt(0);
+	
+	System.out.println("Você digitou: " + x);
+		
+	sc.close();
+
+	}
+
+}
---
+++ estruturaSequencial/src/ScannerDouble.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ScannerDouble {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		double x;
+		x = sc.nextDouble();
+		
+		System.out.printf("Você digitou: %.2f%n", x);
+		
+		sc.close();
+
+	}
+
+}
