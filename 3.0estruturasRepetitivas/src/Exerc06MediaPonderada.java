955949bc57f7f9238ccaea0d362e18e1f04880b0
marcosbarker <marcos_barker@hotmail.com>
2021-08-23 13:32:46 -0300

    code:

--- 3.0estruturasRepetitivas/src/Exerc06MediaPonderada.java
+++ 3.0estruturasRepetitivas/src/Exerc06MediaPonderada.java
@@ -1,3 +1,20 @@
+/*
+Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+peso 5.
+
+Entrada: 
+3
+6.5 4.3 6.2
+5.1 4.2 8.1
+8.0 9.0 10.0
+
+Saída:
+5.7
+6.3
+9.3
+*/
 import java.util.Scanner;
 
 public class Exerc06MediaPonderada {
